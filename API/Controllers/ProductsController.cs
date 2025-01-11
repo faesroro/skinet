1fc3cc6ab801b8e2a649e287a8cdced55e3d9bdc
fabianrojas <fabianrojasrozo@gmail.com>
2025-01-10 19:48:23 -0500

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,27 +1,25 @@
-using System;
-using System.Reflection.Metadata;
+using API.RequestHelpers;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : BaseAPIController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, 
-    string? type, string? sort) 
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
+        [FromQuery]ProductSpecParams specParams) 
     {
-        var spec = new ProductSpecification(brand, type, sort);
+        var spec = new ProductSpecification(specParams);
 
-        var products = await repo.ListAsync(spec);
+        
 
-        return Ok(products);
+        return await CreatePageResult(repo, spec, specParams.PageIndex,specParams.PageSize);
     }
 
     [HttpGet("{id:int}")] //api/products/2
