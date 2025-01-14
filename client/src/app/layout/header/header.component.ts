1433377a0ad5e13c218e9567351c0bd225819432
fabianrojas <fabianrojasrozo@gmail.com>
2025-01-14 16:39:48 -0500

    End of section 8

---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ MatBadgeModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
