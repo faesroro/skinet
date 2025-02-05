7fc244ea7c337085243fcf3750a6cf2996777937
FabianRojas <fabianrojasrozo@gmail.com>
2025-02-05 12:17:41 -0500

    Review section 15

--- client/src/app/core/services/init.service.ts
+++ client/src/app/core/services/init.service.ts
@@ -1,17 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private cartService = inject(CartService);
+  private accountService = inject(AccountService);
 
   init(){
     const cardId = localStorage.getItem('cart_id');
     const cart$ = cardId ? this.cartService.getCart(cardId) : of(null)
 
-    return cart$;
+    return forkJoin({
+      cart: cart$,
+      user: this.accountService.getUserInfo()
+    })
   }
 }
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-header',
@@ -24,4 +25,15 @@ import { CartService } from '../../core/services/cart.service';
 export class HeaderComponent {
   busyService = inject(BusyService);
   cartService = inject(CartService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logOut(){
+    this.accountService.logOut().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      }      
+    })
+  }
 }
